fd4562511eb1b477060f68063b163d3e4b121c11
Rafael Henrique <omaguzin@gmail.com>
2021-11-09 19:29:00 -0300

    Update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     <header className={styles.postHeader}>
       <Link href="/">
         <a>
-          <img src="/logo.svg" alt="Logo" />
+          <img src="/fitlife2.png" alt="Logo" />
         </a>
       </Link>
     </header>
